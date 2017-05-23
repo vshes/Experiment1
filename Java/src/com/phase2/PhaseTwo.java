57defe1041c914d314f2df33b614e35559a58131
vshes <vshes.60@gmail.com>
2017-05-22 22:56:33 -0500

    New File for Phase2 - Method Added

--- Java/src/com/phase2/PhaseTwo.java
+++ Java/src/com/phase2/PhaseTwo.java
@@ -4,4 +4,7 @@ package com.phase2;
  * Created by shesh on 5/22/17.
  */
 public class PhaseTwo {
+    public  void newMethod(){
+        System.out.println("new Method for Phase 2 Added!");
+    }
 }
