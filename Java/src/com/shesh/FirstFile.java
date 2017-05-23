a916db3f54fb4aa8b0376c520aa9530d7b56616e
vshes <vshes.60@gmail.com>
2017-05-22 20:14:12 -0500

    Phase1 Second Commit - Modifed main File.

--- Java/src/com/shesh/FirstFile.java
+++ Java/src/com/shesh/FirstFile.java
@@ -1,11 +1,14 @@
 package com.shesh;
 
+import com.phase1.PhaseOneTest;
+
 /**
  * Created by shesh on 5/22/17.
  */
 public class FirstFile {
     public static void main(String[] args) {
         System.out.println("New file first Commit");
+        new PhaseOneTest().phaseone();
     }
     @Override
     public String toString() {
