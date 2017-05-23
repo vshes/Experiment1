663fb0b0a5995d2b542b44387913f2bf5ff364d7
vshes <vshes.60@gmail.com>
2017-05-22 19:53:28 -0500

    First File Added - new method created .

--- Java/src/com/shesh/FirstFile.java
+++ Java/src/com/shesh/FirstFile.java
@@ -4,4 +4,11 @@ package com.shesh;
  * Created by shesh on 5/22/17.
  */
 public class FirstFile {
+    public static void main(String[] args) {
+        System.out.println("New file first Commit");
+    }
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
