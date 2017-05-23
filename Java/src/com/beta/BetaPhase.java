d065871e8395bf4d1d37560bcea1adeb81c9e6ab
vshes <vshes.60@gmail.com>
2017-05-22 20:38:37 -0500

    Phase Beta First Commit - Added new File.

---
+++ Java/src/com/beta/BetaPhase.java
@@ -0,0 +1,10 @@
+package com.beta;
+
+/**
+ * Created by shesh on 5/22/17.
+ */
+public class BetaPhase {
+    public  void betaphase(){
+        System.out.println("Beta Phase started !");
+    }
+}
